0a3716fe5691f99675a1e3c8dbcf63349441f92c
baberkodextech <73872519+baberkodextech@users.noreply.github.com>
2021-03-04 11:56:44 +0500

    fixes

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -423,7 +423,7 @@ class AuthController extends Controller
         $data['access_token'] = $tokenResult->accessToken;
         $data['token_type'] = 'Bearer';
         $data['expires_at'] = Carbon::parse($tokenResult->token->expires_at)->toDateTimeString();
-        $data['user'] = User::where('id', $request->user()->id)->with('profile')->first();
+        $data['user'] = User::where('id', $request->user()->id)->with('profile.defaultAddress')->first();
         return sendSuccess('Login successfully.', $data);
     }
 
