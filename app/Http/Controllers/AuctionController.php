b616e490c04e380f4f173f9aa3c6e9f0b1ff9108
Aleem71 <aleem@kodextech.com>
2021-06-22 10:16:25 +0500

    Api

--- app/Http/Controllers/AuctionController.php
+++ app/Http/Controllers/AuctionController.php
@@ -816,4 +816,21 @@ class AuctionController extends Controller
         return sendSuccess('Updated',$auctionproduct);
            
     }
+
+    public function getAuctionsDetails(Request $request)
+    {
+
+        $validator = Validator::make($request->all(), [
+            'auction_uuid' => 'required|exists:auctions,uuid',
+        ]);
+
+        if($validator->fails()){
+            $data['validation_error'] = $validator->getMessageBag();
+            return sendError($validator->errors()->all()[0], $data);
+        }
+
+        $auction = Auction::where('uuid', $request->auction_uuid)->with(['medias', 'auction_products','auctioneer'])->first();
+
+        return sendSuccess('Data',$auction); 
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -115,6 +115,7 @@ Route::group(['middleware' => 'auth:api'], function() {
 
     // Auctions
     Route::post('get_auctions', [AuctionController::class, 'getAuctions']);
+    Route::post('get_auctions_details', [AuctionController::class, 'getAuctionsDetails']);
     Route::post('delete_auction', [AuctionController::class, 'deleteAuction']);
     Route::post('delete_auction_product', [AuctionController::class, 'deleteAuctionProduct']);
     Route::post('update_auction', [AuctionController::class, 'updateAuction']);
