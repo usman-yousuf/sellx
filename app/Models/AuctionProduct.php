138dcb6a0c24244672bd493bcc15a1ff98c04014
Aleem71 <aleem@kodextech.com>
2021-06-25 09:49:56 +0500

    Testing

--- app/Models/AuctionProduct.php
+++ app/Models/AuctionProduct.php
@@ -65,7 +65,7 @@ class AuctionProduct extends Model
     //Instance methods
 
     public function getIsReviwedAttribute(){
-        
+        return \Request::User()->profile->id;
         return Reviews::where('auction_product_id',$this->id)->where('sender_profile_id',\Request::User()->profile->id)->first()?1:0;
     }
 
