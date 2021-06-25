5e05d5cd3a1fcc4741c9e15161c4184653f6b7d7
Aleem71 <aleem@kodextech.com>
2021-06-25 18:57:06 +0500

    dsadsa

--- app/Http/Controllers/AuctionController.php
+++ app/Http/Controllers/AuctionController.php
@@ -158,8 +158,9 @@ class AuctionController extends Controller
                 $count=0;
                 $auction = [];
                 foreach ($model as $key =>$value) {
-                    if(!isset($value->following->auction))
+                    if(!isset($value->following->auction)){
                         sendError('NoAuction',[]);
+                    }
 
                     foreach($value->following->auction as $a){
                         if($a->scheduled_date_time >= Carbon::now()){
