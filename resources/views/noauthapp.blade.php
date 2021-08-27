4820dcf13b46008e19e0371f40b4bad54b830800
Aleem71 <aleem@kodextech.com>
2021-08-27 19:33:26 +0500

    cahnges

--- resources/views/noauthapp.blade.php
+++ resources/views/noauthapp.blade.php
@@ -6,7 +6,11 @@
   <!-- CSRF Token -->
   <meta name="csrf-token" content="{{ csrf_token() }}">
 
-  <title>{{ config('app.name', 'Sellx') }}</title>
+  <title> {{ config('app.name', 'Sellx') }}</title>
+  <!-- <link sizes="10x5" rel="shortcut icon" href="{{asset('assets/images/Logo.ico')}}" type="image/x-icon"> -->
+  <link rel="icon" type="image/png" href="http://localhost/sellx/public/assets/images/favicon.png" sizes="16x16" />
+   <meta name="description" content="Sellx is a live auction platform">
+   <meta name="Keywords" content="Sellx is a live auction platform">
 
   <!-- Scripts -->
   <script src="{{ asset('js/app.js') }}" defer></script>
