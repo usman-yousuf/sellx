780b1222bab76db4afb81669a1fb1e63d1e84b29
Aleem71 <aleem@kodextech.com>
2021-08-27 20:08:32 +0500

    changes

--- resources/views/noauthapp.blade.php
+++ resources/views/noauthapp.blade.php
@@ -7,8 +7,8 @@
   <meta name="csrf-token" content="{{ csrf_token() }}">
 
   <title>Sellx is a live auction platform</title>
-  <!-- <link sizes="10x5" rel="shortcut icon" href="{{asset('assets/images/Logo.ico')}}" type="image/x-icon"> -->
-  <link rel="icon" type="image/png" href="http://localhost/sellx/public/assets/images/favicon.png" sizes="16x16" />
+  <link sizes="10x5" rel="shortcut icon" href="{{asset('assets\images\favicon.png')}}" rel="icon" type="image/png">
+  <!-- <link rel="icon" type="image/png" href="http://localhost/sellx/public/assets/images/favicon.png" sizes="16x16" /> -->
    <!-- <meta name="description" content="Sellx is a live auction platform"> -->
    <!-- <meta name="Keywords" content="Sellx is a live auction platform"> -->
 
